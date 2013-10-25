2343ff9df59fe01a6a79eacadbbb27415cc18468
HarisonP <harito92@abv.bg>
2013-10-25 22:00:19 +0300

    First commit

---
+++ application/views/login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+	<link rel="stylesheet" type="text/css" href=''>
+
+</head>
+<body>
+
+<div id="container">
+
+	<div id="body">
+		<div id='login_form'>
+		<form>
+			<label>Username</label>
+			<input id='username' type='text'/></br>
+			<label>Password</label>
+			<input type='password'/></br>
+			<input type='submit'/>
+		</form>
+		</div>
+	</div>
+
+</div>
+
+</body>
+</html>
