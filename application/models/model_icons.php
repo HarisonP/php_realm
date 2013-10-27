5da183251861e08e2b6a2ad8b7ba47356eb6ba63
HarisonP <harito92@abv.bg>
2013-10-27 01:48:29 -0700

    Model for icons created

---
+++ application/models/model_icons.php
@@ -0,0 +1,13 @@
+<?php
+
+class Model_icons extends CI_Model {
+
+    function __construct() {
+        // Call the Model constructor
+        parent::__construct();
+    }
+    function getPositons(){
+    	
+    }
+
+}
